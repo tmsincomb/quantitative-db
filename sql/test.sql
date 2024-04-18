5bc1a94ed2d881fafd2157bf1f3419b3bd74d9fd
Tom Gillespie <tgbugs@gmail.com>
2024-04-18 11:58:12 -0700

    test.sql more tests

--- sql/test.sql
+++ sql/test.sql
@@ -27,3 +27,90 @@ SELECT * FROM  sds_specimen_equiv ORDER BY left_thing;
 SELECT * FROM get_all_equivs(2, 3);
 INSERT INTO sds_specimen_equiv (left_thing, right_thing) VALUES (2, 3);
 SELECT * FROM  sds_specimen_equiv ORDER BY left_thing;
+
+INSERT INTO class_measured (iri, label) VALUES
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/sds/participant', 'participant'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/sds/specimen', 'specimen'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/sds/subject', 'subject'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/sds/sample', 'sample'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/sds/site', 'site'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/sds/performance', 'performance'),
+
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/nerve', 'nerve'), -- nerve or branch
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/nerve/cross-section', 'nerve-cross-section'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/nerve/longitudinal', 'nerve-longitudinal'),
+
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/cell', 'cell'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/neuron', 'neuron'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/soma', 'soma'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/axon', 'axon'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/dendrite', 'dendrite'),
+
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/fascicle', 'fascicle'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/fiber', 'fiber'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/tissue', 'tissue'),
+
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/fascicle/cross-section', 'fascicle-cross-section'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/fiber/cross-section', 'fiber-cross-section'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/tissue/cross-section', 'tissue-cross-section'),
+
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/fascicle/longitudinal', 'fascicle-longitudinal'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/fiber/longitudinal', 'fiber-longitudinal'), -- FIXME axon ??
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/virtual/tissue/longitudinal', 'tissue-longitudinal'),
+
+--('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/', ''),
+--('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/', ''),
+
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/classes/thing', 'class thing')
+;
+
+INSERT INTO controlled_terms (iri, label) VALUES
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/controlled/fascicleStainType/chat', 'chat'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/controlled/fascicleStainType/myelin', 'myelin'),
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/controlled/fascicleStainType/neurofilament', 'neurofilament'),
+
+--('https://uri.interlex.org/tgbugs/uris/readable/quantdb/controlled/', '')
+
+('https://uri.interlex.org/tgbugs/uris/readable/quantdb/controlled/thing', 'controlled thing')
+;
+
+INSERT INTO cat_descriptors (label, is_measuring, range) VALUES
+('fasciclePositiveStainType', (SELECT id from class_measured WHERE label = 'fascicle-longitudinal'),  'controlled'),
+('fasciclePositiveStainType', (SELECT id from class_measured WHERE label = 'fascicle-cross-section'),  'controlled')
+;
+
+INSERT INTO aspects (iri, label) VALUES
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/diameter', 'diameter'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/diameter-orthogonal-to-anterior-posterior-axis', 'diameter-orthogonal-to-anterior-posterior-axis'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/length-parallel-to-anterior-posterior-axis', 'length-parallel-to-anterior-posterior-axis')
+-- ('http://uri.interlex.org/tgbugs/uris/readable/aspect/', ''),
+;
+
+INSERT INTO units (iri, label) VALUES
+-- obvious we need synonyms
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/unit/micrometer', 'um'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/unit/meter', 'm')
+-- ('http://uri.interlex.org/tgbugs/uris/readable/aspect/unit/', ''),
+;
+
+INSERT INTO quant_descriptors (label, is_measuring, aspect, unit, aggregation_type) VALUES
+-- lack of support for hierarchical queries over aspects is a killer here, and also for classes
+('fascicle cross section diameter um',
+(select id from class_measured where label = 'fascicle-cross-section'),
+(select id from aspects where label = 'diameter'),
+(select id from units where label = 'um'),
+'instance'),
+
+('fascicle longitudinal diameter orth-ap-axis um minimum',
+(select id from class_measured where label = 'fascicle-longitudinal'),
+(select id from aspects where label = 'diameter-orthogonal-to-anterior-posterior-axis'), -- and here we see the problem
+(select id from units where label = 'um'),
+'min'),
+
+('fascicle longitudinal length parallel-to-ap-axis um',
+(select id from class_measured where label = 'fascicle-longitudinal'),
+(select id from aspects where label = 'length-parallel-to-anterior-posterior-axis'),
+(select id from units where label = 'um'),
+'instance')
+
+;
