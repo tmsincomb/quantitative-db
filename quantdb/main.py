c3ea7a773d826e784c73a8beaef8aa7977238063
Tom Gillespie <tgbugs@gmail.com>
2024-05-06 20:19:40 -0700

    add main.py for fastapi

---
+++ quantdb/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+from quantdb.api_server import app as flask_app
+
+app = FastAPI()
+
+app.mount('/', WSGIMiddleware(flask_app))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("quantdb.main:app", host='127.0.0.1', port=8990, reload=True)
