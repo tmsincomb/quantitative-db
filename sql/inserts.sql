199b8f237a2780081c73f7c049d6e8b39ebc41a2
Tom Gillespie <tgbugs@gmail.com>
2025-07-26 22:32:40 -0700

    inserts.sql placeholder aspects, test.sql volume/cross-section hierarchy
    the 80% solution for aspect-of-type-in-context is to add a single
    additional desc_inst column to the desc quant table to make it
    possible to do counts and areas and other aspects of the population of
    said thing in the context of the instance that the number is attached
    to, it doesn't work for everything but it gets is much closer, in
    theory the same field can be used to enable the reverse where the
    aspect is attached to the child and the context is the parent, so
    position of fiber in fascicle and the direction is inferred from the
    partonomy over the instances
    
    in the process it also became clear that units should be part of
    values NOT part of quantiative descriptors, since (beyond the fact
    that quantitative values should _always_ have units and numbers should
    always travel with units, re: mars climate orbiter failure of having
    unitless numbers that got read in incorrectly despite the fact that
    the system had proper typing internally) they are the instance
    equivalent for aspects
    
    class -> instance (aka desc_inst -> instance)
    aspect -> unit
    
    in the mean time adding placeholder aspects that materialize the thing
    being counted and using aggregation type summary to distinguish them,
    thus the aspects hierarchy is complicated by the duplication of what
    should be in the instance hierachy, but ok for now
    
    in theory we may want to ingest the source identifier (when available)
    as a quantiative value (technially an arbitrary pointer to the inst)
    so that it can be used as a sanity check inside the db for id_formal
    in values_inst

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -128,6 +128,9 @@ INSERT INTO units (iri, label) VALUES
 
 INSERT INTO aspects (iri, label) VALUES
 -- technically position is a vector of distances from a defined starting point or in a defined coordinate system
+
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/identifier/instance', 'identifier-instance'),
+
 ('http://uri.interlex.org/tgbugs/uris/readable/aspect/position', 'position'), -- XXX this is always relative to something that needs to be specified
 ('http://uri.interlex.org/tgbugs/uris/readable/aspect/centroid', 'centroid'),
 -- distance
@@ -143,33 +146,86 @@ INSERT INTO aspects (iri, label) VALUES
 ('http://uri.interlex.org/tgbugs/uris/readable/aspect/volume', 'volume'),
 
 ('http://uri.interlex.org/tgbugs/uris/readable/aspect/angle', 'angle'),
-('http://uri.interlex.org/tgbugs/uris/readable/aspect/angle/from/parent/centroid', 'angle-from-parent-centroid') -- FIXME -from- implies we need context for these
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/angle/from/parent/centroid', 'angle-from-parent-centroid'), -- FIXME -from- implies we need context for these
+
+-- placeholder hierarchy until i can update desc quant to allow a second desc_inst
+
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber', 'count-fiber'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/a', 'count-fiber-a'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/a/alpha', 'count-fiber-a-alpha'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/a/beta', 'count-fiber-a-beta'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/a/gamma', 'count-fiber-a-gamma'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/a/delta', 'count-fiber-a-delta'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/b', 'count-fiber-b'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/c', 'count-fiber-c'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/myelinated', 'count-fiber-myelinated'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/unmyelinated', 'count-fiber-unmyelinated'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/chat', 'count-fiber-chat'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/count/fiber/nav', 'count-fiber-nav'),
+
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber', 'area-fiber'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/a', 'area-fiber-a'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/a/alpha', 'area-fiber-a-alpha'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/a/beta', 'area-fiber-a-beta'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/a/gamma', 'area-fiber-a-gamma'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/a/delta', 'area-fiber-a-delta'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/b', 'area-fiber-b'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/c', 'area-fiber-c'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/myelinated', 'area-fiber-myelinated'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/unmyelinated', 'area-fiber-unmyelinated'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/chat', 'area-fiber-chat'),
+('http://uri.interlex.org/tgbugs/uris/readable/aspect/area/fiber/nav', 'area-fiber-nav')
 
 ;
 
-INSERT INTO aspect_parent (id, parent) VALUES
-(aspect_from_label('radius'), aspect_from_label('distance')),
-(aspect_from_label('perimeter'), aspect_from_label('distance')),
-(aspect_from_label('angle-from-parent-centroid'), aspect_from_label('angle')),
-(aspect_from_label('radius-from-parent-centroid'), aspect_from_label('radius')),
+INSERT INTO aspect_parent (parent, id) VALUES
+(aspect_from_label('distance'), aspect_from_label('radius')),
+(aspect_from_label('distance'), aspect_from_label('perimeter')),
+(aspect_from_label('angle'), aspect_from_label('angle-from-parent-centroid')),
+(aspect_from_label('radius'), aspect_from_label('radius-from-parent-centroid')),
 
-(aspect_from_label('centroid'), aspect_from_label('position')),
-(aspect_from_label('distance-along-axis'), aspect_from_label('distance')),
-(aspect_from_label('centroid-x'), aspect_from_label('distance-along-axis')), -- technically correct but annoying to find due to needing partOf or similar
-(aspect_from_label('centroid-y'), aspect_from_label('distance-along-axis')) -- and due to lacking vectors atm
+(aspect_from_label('position'), aspect_from_label('centroid')),
+(aspect_from_label('distance'), aspect_from_label('distance-along-axis')),
+(aspect_from_label('distance-along-axis'), aspect_from_label('centroid-x')), -- technically correct but annoying to find due to needing partOf or similar
+(aspect_from_label('distance-along-axis'), aspect_from_label('centroid-y')), -- and due to lacking vectors atm
+
+-- XXX placeholders
+
+(aspect_from_label('count'), aspect_from_label('count-fiber')),
+(aspect_from_label('count-fiber'), aspect_from_label('count-fiber-a')),
+(aspect_from_label('count-fiber-a'), aspect_from_label('count-fiber-a-alpha')),
+(aspect_from_label('count-fiber-a'), aspect_from_label('count-fiber-a-beta')),
+(aspect_from_label('count-fiber-a'), aspect_from_label('count-fiber-a-gamma')),
+(aspect_from_label('count-fiber-a'), aspect_from_label('count-fiber-a-delta')),
+(aspect_from_label('count-fiber'), aspect_from_label('count-fiber-b')),
+(aspect_from_label('count-fiber'), aspect_from_label('count-fiber-c')),
+(aspect_from_label('count-fiber'), aspect_from_label('count-fiber-myelinated')),
+(aspect_from_label('count-fiber'), aspect_from_label('count-fiber-unmyelinated')),
+(aspect_from_label('count-fiber'), aspect_from_label('count-fiber-chat')),
+(aspect_from_label('count-fiber'), aspect_from_label('count-fiber-nav')),
+
+(aspect_from_label('area'), aspect_from_label('area-fiber')),
+(aspect_from_label('area-fiber'), aspect_from_label('area-fiber-a')),
+(aspect_from_label('area-fiber-a'), aspect_from_label('area-fiber-a-alpha')),
+(aspect_from_label('area-fiber-a'), aspect_from_label('area-fiber-a-beta')),
+(aspect_from_label('area-fiber-a'), aspect_from_label('area-fiber-a-gamma')),
+(aspect_from_label('area-fiber-a'), aspect_from_label('area-fiber-a-delta')),
+(aspect_from_label('area-fiber'), aspect_from_label('area-fiber-b')),
+(aspect_from_label('area-fiber'), aspect_from_label('area-fiber-c')),
+(aspect_from_label('area-fiber'), aspect_from_label('area-fiber-myelinated')),
+(aspect_from_label('area-fiber'), aspect_from_label('area-fiber-unmyelinated')),
+(aspect_from_label('area-fiber'), aspect_from_label('area-fiber-chat')),
+(aspect_from_label('area-fiber'), aspect_from_label('area-fiber-nav'))
+
+--(aspect_from_label('parent'), aspect_from_label('child')),
+
 ;
 
 INSERT INTO descriptors_quant (label, domain, aspect, unit, aggregation_type) VALUES
 
-('fascicle cross section area um2',
-desc_inst_from_label('fascicle-cross-section'),
-aspect_from_label('diameter'),
-unit_from_label('um2'),
-'instance'),
-
 ('fiber cross section area um2',
 desc_inst_from_label('fiber-cross-section'),
-aspect_from_label('diameter'),
+aspect_from_label('area'),
 unit_from_label('um2'),
 'instance'),
 
@@ -189,7 +245,195 @@ unit_from_label('um'),
 desc_inst_from_label('fiber-cross-section'),
 aspect_from_label('diameter'),
 unit_from_label('um'),
-'max')
+'max'),
+
+-- aspect of type in context
+
+('fiber count in fascicle cross section',
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-nf'),
+null,
+'instance'),
+
+-- address: fascicle
+-- XXX (is desc_inst or not actually quantitative but instead an identifer that also happens to be a quantity)
+
+('fascicle cross section area um2', -- address: area
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area'),
+unit_from_label('um2'),
+'instance'),
+('fascicle cross section diameter um max', -- address: longest_diameter
+desc_inst_from_label('fascicle-cross-section'), -- also in test.sql
+aspect_from_label('diameter'),
+unit_from_label('um'),
+'max'),
+('fascicle cross section diameter um min', -- address: shortest_diameter
+desc_inst_from_label('fascicle-cross-section'), -- also in test.sql
+aspect_from_label('diameter'),
+unit_from_label('um'),
+'min'),
+('fascicle cross section diameter um', -- address: eff_diam
+desc_inst_from_label('fascicle-cross-section'), -- also in test.sql FIXME equivalent area circle diameter ...
+aspect_from_label('diameter'),
+unit_from_label('um'),
+'instance'),
+('nav fiber count in fascicle cross section estimated', -- address: c_estimate_nav
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber'),
+null,
+'summary'),
+('fiber count in fascicle cross section estimated', -- address: c_estimate_nf
+desc_inst_from_label('fascicle-cross-section'), -- FIXME not sure how to interpret this though ... also nf nerve fiber or neurofilament?
+aspect_from_label('count-estimated-fiber'),
+null,
+'summary'),
+-- address: nfibers_w_c_estimate_nav
+-- address: nfibers_w_c_estimate_nf
+('fiber count in fascicle cross section', -- address: nfibers_all
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber'),
+null,
+'summary'),
+
+-- fiber counts
+
+('a fiber count in fascicle cross section',
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-a'),
+null,
+'summary'),
+('alpha a fiber count in fascicle cross section', -- address: n_a_alpha
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-a-alpha'),
+null,
+'summary'),
+('beta a fiber count in fascicle cross section', -- address: n_a_beta
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-a-beta'),
+null,
+'summary'),
+('gamma a fiber count in fascicle cross section', -- address: n_a_gamma
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-a-gamma'),
+null,
+'summary'),
+('delta a fiber count in fascicle cross section', -- address: n_a_delta
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-a-delta'),
+null,
+'summary'),
+('b fiber count in fascicle cross section', -- address: n_b
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-b'),
+null,
+'summary'),
+('unmyelinated fiber count in fascicle cross section', -- address: n_unmyel_nf
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-unmyelinated'),
+null,
+'summary'),
+('nav fiber count in fascicle cross section', -- address: n_nav
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-nav'),
+null,
+'summary'),
+('chat fiber count in fascicle cross section', -- address: n_chat
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-chat'),
+null,
+'summary'),
+('myelinated fiber count in fascicle cross section', -- address: n_myelinated
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('count-fiber-myelinated'),
+null,
+'summary'),
+
+-- fiber areas
+
+('a fiber area in fascicle cross section um2',
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-a'),
+unit_from_label('um2'),
+'summary'),
+('alpha a fiber area in fascicle cross section um2', -- address: area_a_alpha
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-a-alpha'),
+unit_from_label('um2'),
+'summary'),
+('beta a fiber area in fascicle cross section um2', -- address: area_a_beta
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-a-beta'),
+unit_from_label('um2'),
+'summary'),
+('gamma a fiber area in fascicle cross section um2', -- address: area_a_gamma
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-a-gamma'),
+unit_from_label('um2'),
+'summary'),
+('delta a fiber area in fascicle cross section um2', -- address: area_a_delta
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-a-delta'),
+unit_from_label('um2'),
+'summary'),
+('b fiber area in fascicle cross section um2', -- address: area_b
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-b'),
+unit_from_label('um2'),
+'summary'),
+('unmyelinated fiber area in fascicle cross section um2', -- address: area_unmyel_nf
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-unmyelinated'),
+unit_from_label('um2'),
+'summary'),
+('nav fiber area in fascicle cross section um2', -- address: area_nav
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-nav'),
+unit_from_label('um2'),
+'summary'),
+('chat fiber area in fascicle cross section um2', -- address: area_chat
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-chat'),
+unit_from_label('um2'),
+'summary'),
+('myelinated fiber area in fascicle cross section um2', -- address: area_myelinated
+desc_inst_from_label('fascicle-cross-section'),
+aspect_from_label('area-fiber-myelinated'),
+unit_from_label('um2'),
+'summary'),
+
+-- address: crop_x_start
+-- address: crop_x_stop
+-- address: crop_y_start
+-- address: crop_y_stop
+-- address: chat_available
+-- address: nav_available
+-- address: th_available
+-- address: x_pix
+-- address: y_pix
+-- address: x_um
+-- address: y_um
+-- address: x_cent
+-- address: y_cent
+-- address: rho
+-- address: rho_pix
+-- address: phi
+-- address: epi_dist
+-- address: epi_dist_inv
+-- address: nerve_based_area
+-- address: nerve_based_perimeter
+-- address: nerve_based_eff_diam
+-- address: perinerium_vertices
+-- address: perinerium_vertices_px
+-- address: nerve_based_shortest_diameter
+-- address: hull_contrs
+-- address: hull_contr_areas
+
+('instance identifier', -- yes it sort of works but is it really a quantitative value about the thing? no it is just a pointer to the thing in some system
+desc_inst_from_label('class thing'),
+aspect_from_label('identifier-instance'),
+null,
+'instance')
 
 ;
 
